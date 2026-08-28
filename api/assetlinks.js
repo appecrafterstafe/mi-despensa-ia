755f3e35b602b49e3b92a4295ec7879aa4228ff7
appecrafterstafe <appecrafter.stafe@gmail.com>
2026-08-28 00:28:19 -0300

    Update assetlinks.js

--- api/assetlinks.js
+++ api/assetlinks.js
@@ -6,7 +6,7 @@ export default function handler(req, res) {
       target: {
         namespace: "android_app",
         package_name: "app.vercel.mi_despensa_ia.twa",
-        sha256_cert_fingerprints: ["99:84:1F:FF:10:BF:51:F5:8D:5C:BA:8F:E8:56:06:5D:E9:31:95:2C:96:A6:71:54:D1:D0:71:73:0B..."]
+        sha256_cert_fingerprints: ["99:84:1F:FF:10:BF:51:F5:8D:5C:BA:8F:E8:56:06:5D:E9:31:95:2C:96:A6:71:54:D1:D0:71:73:0B:49:4A:C4"]
       }
     }
   ]);
