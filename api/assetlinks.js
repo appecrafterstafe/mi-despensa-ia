bf177d724896c1bed59aadfc19761cc7022e7fd2
appecrafterstafe <appecrafter.stafe@gmail.com>
2026-08-28 00:31:51 -0300

    Update package name and SHA256 fingerprint

--- api/assetlinks.js
+++ api/assetlinks.js
@@ -5,8 +5,8 @@ export default function handler(req, res) {
       relation: ["delegate_permission/common.handle_all_urls"],
       target: {
         namespace: "android_app",
-        package_name: "app.vercel.mi_despensa_ia.twa",
-        sha256_cert_fingerprints: ["99:84:1F:FF:10:BF:51:F5:8D:5C:BA:8F:E8:56:06:5D:E9:31:95:2C:96:A6:71:54:D1:D0:71:73:0B:49:4A:C4"]
+        package_name: "app.vercel.mi_despensa_ia_nine.twa",
+        sha256_cert_fingerprints: ["99:84:1F:FF:10:BF:51:F5:BD:5C:BA:8F:E8:56:06:5D:E9:31:95:2C:96:A6:71:54:D1:D0:71:73:0B:49:4A:C4"]
       }
     }
   ]);
